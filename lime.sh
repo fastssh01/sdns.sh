41887cb5a97dbd1d358cdf124d3c7bee1a8d9604
fastssh01 <151945684+fastssh01@users.noreply.github.com>
2023-12-09 18:25:06 +0800

    Create lime.sh

---
+++ lime.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Set your preferred DNS server
+DNS_SERVER="8.8.8.8"
+
+# Function to perform DNS query
+perform_dns_query() {
+  local result
+  result=$(dig +stats +noquestion +nocomments +noauthority +noadditional +nocmd +nostats @$DNS_SERVER "$1")
+  echo "$result"
+}
+
+# Function to display results in a box
+display_results() {
+  local domain=$1
+  local result=$(perform_dns_query $domain)
+
+  if [[ $result == *"Query time"* ]]; then
+    echo -e "\e[92mDNS query for $domain was successful:\e[0m"
+  else
+    echo -e "\e[91mDNS query for $domain failed.\e[0m"
+  fi
+
+  # Output the results in a box
+  echo -e "┌──────────────────────────────────────┐"
+  echo -e "│ $result │"
+  echo -e "└──────────────────────────────────────┘"
+}
+
+# Continuous optimization loop
+while true; do
+  # Manual input for domain
+  echo -e "Enter the domain you want to query (or 'exit' to quit):"
+  read domain
+
+  # Check if the user wants to exit
+  if [ "$domain" == "exit" ]; then
+    echo "Exiting the optimization loop."
+    break
+  fi
+
+  # Perform DNS query and display results
+  display_results $domain
+done
